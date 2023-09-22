eb1d10ad9b92c16129368119848010ba40be1b7d
lui <larauz@cendrassos.net>
2023-09-22 16:59:40 +0200

    Segona part, afegim musisca afegim reproductor,informacio de la canço

--- app.js
+++ app.js
@@ -1,15 +1,84 @@
 let pausebtn = document.getElementById("pause");
 let play = document.getElementById("play");
 let imgChange = document.getElementById("imgchange");
-
+var coverimg = document.getElementById("cover")
 var playorpause = true;
+let next = document.getElementById("next");
+const music = document.createElement("audio")
+
+var plst1 = document.getElementById("plst1")
+var plst2 = document.getElementById("plst2")
 // funcion para poder pausar o reproducir musica (true es reproducir ) 
 pausebtn.onclick = function(){
     if (playorpause){
         imgChange.src = "./img/pauseb.png"
         playorpause = false;
+        music.play();
     }else{
+        music.pause();
         imgChange.src = "./img/playb.png";
         playorpause = true;
     }
 }
+
+
+var lI = document.getElementById('llistamusica');
+
+
+
+
+
+var currentPlaylist;
+
+
+function loadAndShowPlaylist() {
+    // Limpia la lista de reproducción actual
+    llistamusica.innerHTML = '';
+    
+    // Crea una lista desordenada (ul)
+    var ul = document.createElement('ul');
+
+    // Recorre los elementos en la lista de reproducción actual y crea elementos de lista (li) para cada canción
+    currentPlaylist.forEach(function(song) {
+        var li = document.createElement('li');
+        li.innerHTML = '<strong>' + song.title + '</strong> - ' + song.artist;
+
+        // Agrega un evento de clic a cada elemento de la lista
+        li.addEventListener('click', function() {
+            selectedS = song
+            showSelectedSongDetails(song);
+        });
+
+        // Añade el elemento de lista a la lista desordenada
+        ul.appendChild(li);
+    });
+ 
+    // Añade la lista desordenada al div de la lista de reproducción
+    llistamusica.appendChild(ul);
+
+    // Muestra la primera canción de la lista de reproducción actual (si existe)
+    if (currentPlaylist.length > 0) {
+        showSelectedSongDetails(currentPlaylist[0]);
+    }
+}
+
+
+// Función para mostrar los detalles de la canción seleccionada
+function showSelectedSongDetails(song) {
+    coverimg.src = song.cover;
+    music.src = song.url;   
+}
+
+// Asigna eventos de clic a los botones de las playlists
+plst1.addEventListener('click', function() {
+    currentPlaylist = llistaI;
+    loadAndShowPlaylist();
+});
+
+plst2.addEventListener('click', function() {
+    currentPlaylist = llistaII;
+    loadAndShowPlaylist();
+});
+
+// Carga y muestra la primera lista de reproducción inicialmente
+loadAndShowPlaylist();
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+        $playlist1 = file_get_contents("./llista1.json");
+        $cancionPlay1 = json_decode($playlist1, true);
+        $playlist2 = file_get_contents("./llista2.json");
+        $cancionPlay2 = json_decode($playlist2, true);
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,37 +17,47 @@
        
         <div class="container">
             <div class="Nav">
-              <button class="Llista 1"></button>
-              <button class="Llista 2"></button>
-              <button class="Llista 3"></button>
-              <button class="Llista 4"></button>
+              <button class="Llista" id="plst1">Un poco de todo</button>
+              <button class="Llista" id="plst2">Rock</button>
+              <button class="Llista"></button>
+              <button class="Llista"></button>
               <button class="Afegir Llista+">Afegir Llista+</button>
             </div>
             <div class="Body">
               <div class="Art-Box">
-                <div class="caratula"></div>
+                <div id="llistamusica">
+                
+                </div>
+                <div class="caratula">
+                    <img id="cover" alt="">
+                </div>
               </div>
               <div class="Status-Box"></div>
               
               <div class="Controls-Box">
                 <Button class="controls" >
-                  <img src="./img/stop.png" alt="">
+                  <img src="./img/stop.png">
                 </Button>
                 <Button class="controls" >
                   <img src="./img/back.png" alt="">
                 </Button>
                 <Button class="controls" id="pause">
-                  <img src="./img/playb.png" id="imgchange" alt="">
+                  <img src="./img/playb.png" id="imgchange">
+                </Button>
+                <Button class="controls" id="next">
+                  <img src="./img/next.png">
                 </Button>
                 <Button class="controls" >
-                  <img src="./img/next.png" alt="">
-                </Button>
-                <Button class="controls" >
-                  <img src="./img/random.png" alt="">
+                  <img src="./img/random.png">
                 </Button>
               </div>
             </div>
           </div>
+          <script>
+            var llistaI = <?php echo json_encode($cancionPlay1); ?>;
+            var llistaII = <?php echo json_encode($cancionPlay2); ?>;
+            </script>
           <script src="./app.js"></script>
+          
     </body>
 </html>
